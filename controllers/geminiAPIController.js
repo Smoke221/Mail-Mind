a34a600ee48bf91a00d0fcff2c941e858a4abd74
Anil <kanilreddy867@gmail.com>
2024-04-01 16:10:53 +0530

    renamed main file so it works with backend deployement.

--- controllers/geminiAPIController.js
+++ controllers/geminiAPIController.js
@@ -1,13 +1,13 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require("dotenv").config();
 
-// Access your API key as an environment variable
+
 const apiKey = process.env.GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 async function generateText(prompt) {
   try {
-    // For text-only input, use the gemini-pro model
+
     const model = genAI.getGenerativeModel({ model: "gemini-pro" });
 
     const result = await model.generateContent(prompt);
